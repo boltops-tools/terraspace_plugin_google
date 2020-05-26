4afc940e4519fe55c604ef30f1a265133cf98003
Tung Nguyen <tongueroo@gmail.com>
2020-05-26 23:16:54 +0000

    add example templates

--- lib/terraspace_provider_gcp.rb
+++ lib/terraspace_provider_gcp.rb
@@ -2,7 +2,7 @@ lib = File.expand_path("../../../", __FILE__)
 $:.unshift(lib)
 
 require "memoist"
-require "terraspace" # for base classes
+require "terraspace" # for interface
 
 require "terraspace_provider_gcp/version"
 require "terraspace_provider_gcp/autoloader"
@@ -12,4 +12,7 @@ module TerraspaceProviderGcp
   class Error < StandardError; end
 end
 
-Terraspace::Provider.register("gcs", "gcp")
+Terraspace::Provider.register("gcp",
+  backend: "gcs",
+  root: File.dirname(__dir__)
+)
--- lib/terraspace_provider_gcp/autoloader.rb
+++ lib/terraspace_provider_gcp/autoloader.rb
@@ -10,16 +10,13 @@ module TerraspaceProviderGcp
     end
 
     class << self
-      @@already_setup = false
       def setup
-        return if @@already_setup
         loader = Zeitwerk::Loader.new
         loader.inflector = Inflector.new
         lib = File.expand_path("../", __dir__)
         loader.push_dir(lib)
         loader.ignore("#{lib}/terraspace_provider_gcp.rb")
         loader.setup
-        @@already_setup = true
       end
     end
   end
