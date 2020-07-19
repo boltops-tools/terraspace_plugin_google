c3f64a0ef977295d1480ede5c4fc0427bc60fd88
Tung Nguyen <tongueroo@gmail.com>
2020-07-19 21:06:50 +0000

    use this in example

--- lib/templates/test/rspec/module/test/spec/fixtures/stack/main.tf
+++ lib/templates/test/rspec/module/test/spec/fixtures/stack/main.tf
@@ -1,10 +1,10 @@
-resource "random_pet" "bucket" {
+resource "random_pet" "this" {
   length = 2
 }
 
 module "bucket" {
   source = "../../modules/example"
 
-  name               = "bucket-${random_pet.bucket.id}"
+  name               = "bucket-${random_pet.this.id}"
   bucket_policy_only = var.bucket_policy_only
 }
