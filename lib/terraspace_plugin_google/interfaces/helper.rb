75e95a76abd294cb9a1b61e639b8b708603b7416
Tung Nguyen <tongueroo@gmail.com>
2020-11-15 02:21:08 +0000

    cache_helper

--- lib/terraspace_plugin_google/interfaces/helper.rb
+++ lib/terraspace_plugin_google/interfaces/helper.rb
@@ -1,7 +1,10 @@
 module TerraspacePluginGoogle::Interfaces
   module Helper
+    include Terraspace::Plugin::Helper::Interface
+
     def google_secret(name, options={})
       Secret.new(options).fetch(name)
     end
+    cache_helper :google_secret
   end
 end
